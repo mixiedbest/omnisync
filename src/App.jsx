08fccb686adc7f8863a8e89995a7928cbf291594
Mixie <mixiemgmt@gmail.com>
2025-12-08 21:36:09 -0600

    Add contact button to home page
    - Button was only on some pages
    - Now appears on home page
    - Should be visible in bottom right corner

--- src/App.jsx
+++ src/App.jsx
@@ -321,6 +321,23 @@ function App() {
           onVolumeChange={setVolume}
           currentTrack={displayTrack}
         />
+
+        {/* Contact Form */}
+        {showContactForm && (
+          <ContactForm onClose={() => setShowContactForm(false)} />
+        )}
+
+        {/* Contact Button */}
+        <button
+          className="contact-button"
+          onClick={() => setShowContactForm(true)}
+          title="Contact Us"
+        >
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+            <polyline points="22,6 12,13 2,6" />
+          </svg>
+        </button>
       </>
     );
   }
